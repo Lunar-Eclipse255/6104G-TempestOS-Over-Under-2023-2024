b971ed4a7628647edc8e34fd413e91c72ac96620
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-09-08 20:14:25 -0400

    cheesy drive

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include "main.h"
 #include "display.hpp"
 #include "autons.hpp"
+# define M_PI           3.14159265358979323846
 //#define INDEX_PORT 'A'
 //#define ENDGAME_PORT 'C'
+/**
+ * Cheesy Drive Constants
+ */
+#define DRIVE_DEADBAND 0.1f
+#define DRIVE_SLEW 0.02f
+#define CD_TURN_NONLINEARITY                                                   \
+  0.65 // This factor determines how fast the wheel
+       // traverses the "non linear" sine curve
+#define CD_NEG_INERTIA_SCALAR 4.0
+#define CD_SENSITIVITY 1.0
 
 
 
@@ -21,6 +32,88 @@ void on_center_button() {
 	}
 }
 
+// We apply a sinusoidal curve (twice) to the joystick input to give finer
+// control at small inputs.
+static double _turnRemapping(double iturn) {
+	double denominator = sin(M_PI / 2 * CD_TURN_NONLINEARITY);
+	double firstRemapIteration =
+	    sin(M_PI / 2 * CD_TURN_NONLINEARITY * iturn) / denominator;
+	return sin(M_PI / 2 * CD_TURN_NONLINEARITY * firstRemapIteration) / denominator;
+}
+
+// On each iteration of the drive controller (where we aren't point turning) we
+// constrain the accumulators to the range [-1, 1].
+double quickStopAccumlator = 0.0;
+double negInertiaAccumlator = 0.0;
+static void _updateAccumulators() {
+	if (negInertiaAccumlator > 1) {
+		negInertiaAccumlator -= 1;
+	} else if (negInertiaAccumlator < -1) {
+		negInertiaAccumlator += 1;
+	} else {
+		negInertiaAccumlator = 0;
+	}
+
+	if (quickStopAccumlator > 1) {
+		quickStopAccumlator -= 1;
+	} else if (quickStopAccumlator < -1) {
+		quickStopAccumlator += 1;
+	} else {
+		quickStopAccumlator = 0.0;
+	}
+}
+
+double prevTurn = 0.0;
+double prevThrottle = 0.0;
+std::pair<double, double> cheesyDrive(double ithrottle, double iturn) {
+	bool turnInPlace = false;
+	double linearCmd = ithrottle;
+	if (fabs(ithrottle) < DRIVE_DEADBAND && fabs(iturn) > DRIVE_DEADBAND) {
+		// The controller joysticks can output values near zero when they are
+		// not actually pressed. In the case of small inputs like this, we
+		// override the throttle value to 0.
+		linearCmd = 0.0;
+		turnInPlace = true;
+	} else if (ithrottle - prevThrottle > DRIVE_SLEW) {
+		linearCmd = prevThrottle + DRIVE_SLEW;
+	} else if (ithrottle - prevThrottle < -(DRIVE_SLEW * 2)) {
+		// We double the drive slew rate for the reverse direction to get
+		// faster stopping.
+		linearCmd = prevThrottle - (DRIVE_SLEW * 2);
+	}
+
+	double remappedTurn = _turnRemapping(iturn);
+
+	double left, right;
+	if (turnInPlace) {
+		// The remappedTurn value is squared when turning in place. This
+		// provides even more fine control over small speed values.
+		left = remappedTurn * std::abs(remappedTurn);
+		right = -remappedTurn * std::abs(remappedTurn);
+
+	} else {
+		double negInertiaPower = (iturn - prevTurn) * CD_NEG_INERTIA_SCALAR;
+		negInertiaAccumlator += negInertiaPower;
+
+		double angularCmd =
+		    abs(linearCmd) *  // the more linear vel, the faster we turn
+		        (remappedTurn + negInertiaAccumlator) *
+		        CD_SENSITIVITY -  // we can scale down the turning amount by a
+		                          // constant
+		    quickStopAccumlator;
+
+		right = left = linearCmd;
+		left += angularCmd;
+		right -= angularCmd;
+
+		_updateAccumulators();
+	}
+
+	prevTurn = iturn;
+	prevThrottle = ithrottle;
+	
+	return std::make_pair(left, right);
+}
 
 
 MotorGroup rightChassis ({1, 2, 11});
@@ -99,14 +192,19 @@ void opcontrol() {
 
 	while (true) {
 		// Arcade drive with the left stick.
-		double leftJoystickInput = controller.getAnalog(okapi::ControllerAnalog::leftY);
-		double rightJoystickInput = controller.getAnalog(okapi::ControllerAnalog::rightY);
+		// Read input values for throttle and turn from your input device (e.g., joystick)
+        double throttle = controller.getAnalog(ControllerAnalog::leftY);
+        double turn = controller.getAnalog(ControllerAnalog::rightX);
 
-		double leftScaledValue = pow(leftJoystickInput, 3);   
-		double rightScaledValue = pow(rightJoystickInput, 3); 
-		leftChassis.moveVoltage(leftScaledValue * 12000);   
-		rightChassis.moveVoltage(rightScaledValue * 12000); 
-		drive->getModel()->arcade(leftChassis.getVoltage(), rightChassis.getVoltage());
+        // Calculate motor outputs using the cheesyDrive function
+        std::pair<double, double> motorOutputs = cheesyDrive(throttle, turn);
+
+        // Set the left and right motor voltages using the calculated outputs
+        drive->getModel()->arcade(
+            motorOutputs.first * 12000.0, // Scale the outputs to the motor voltage range
+            motorOutputs.second * 12000.0
+        );
+		drive->getModel()->tank(leftChassis.getVoltage(), rightChassis.getVoltage());
 
 
 
@@ -126,7 +224,7 @@ void opcontrol() {
 
     // Print the drivetrain velocity to the PROS terminal (or LCD screen)
     pros::screen::set_pen(COLOR_RED);
-    pros::screen::print(pros::E_TEXT_MEDIUM, 1, "%d", drivetrainVelocity);
+    //pros::screen::print(pros::E_TEXT_MEDIUM, 1, "%d", scaledRightX);
         // Print the motor velocity to the controller's display on line 3
 	if (catapultLimit.isPressed()) {
     	catapultMotor.moveVelocity(0);
