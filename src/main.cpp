a4f8c6cbc6d1ab4ee01ab848f4178fd659e2d449
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-10-12 00:39:16 -0400

    chanfes

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include "display.hpp"
 #include "autons.hpp"
 #include "motors.h"
-//#define CATA_PORT 'A'
-//#define ENDGAME_PORT 'C'
+#define WING_LEFT 'B'
+#define WING_RIGHT 'C'
+#define ARM 'D'
 
 
 
@@ -22,25 +23,30 @@ double motorVelocityCalc(double joystickInput) {
     motorVelocity = std::min(100.0, std::max(-100.0, motorVelocity));
 
     //Adds the direction back to the motor power
+	if (selectedProfile==0){
     return (joystickInput < 0) ? -motorVelocity : motorVelocity;
+	}
+	else {
+		return (joystickInput < 0) ? motorVelocity : -motorVelocity;
+	}
+
 }
 
 double turningValueCalc(double joystickInput) {
     //Coefficients for Quartic model
-    double a = 5.0; 
+    double a = 3.0; 
     double b = 1.0; 
 	double c = 2.0; 
 	double d = 0.0; 
 	double e = 0.0; 
 
     //motorVelocity = ax^4+ bx^3 + cx^2 + dx + e, where x is the joystick value: Quartic Linear regression model
-    double turningValue = (a * pow(joystickInput, 4)) + (b * pow(joystickInput, 3)) + (c * pow(joystickInput, 2))+ (d * joystickInput)+e;
+    double turningValue = (a * pow(joystickInput, 3)) + (b * pow(joystickInput, 3)) + (c * pow(joystickInput, 2))+ (d * joystickInput)+e;
     //Adjusts value to fit into expected input value 
     turningValue = std::min(1.0, std::max(-1.0, turningValue));
-	return (joystickInput < 0) ? -turningValue : turningValue;
 
 	//returns the turningValue
-    return turningValue;
+	return -turningValue;
 }
 
 //A callback function for LLEMU's center button. When this callback is fired, it will toggle line 2 of the LCD text between "I was pressed!" and nothing. 
@@ -54,19 +60,18 @@ void on_center_button() {
 	}
 }
 
-
 Motor backLeftDriveMotor (20);
 Motor middleLeftDriveMotor (19);
 Motor frontLeftDriveMotor (18);
-Motor upLeftDriveMotor (17);
+Motor upLeftDriveMotor (-17);
 Motor backRightDriveMotor (-10);
 Motor middleRightDriveMotor (-9);
 Motor frontRightDriveMotor (-8);
-Motor upRightDriveMotor (-7);
-MotorGroup leftChassis ({backLeftDriveMotor,middleLeftDriveMotor,frontLeftDriveMotor});
-MotorGroup rightChassis ({backRightDriveMotor, middleRightDriveMotor, frontRightDriveMotor});
+Motor upRightDriveMotor (7);
+MotorGroup leftChassis ({backLeftDriveMotor,middleLeftDriveMotor,frontLeftDriveMotor, upLeftDriveMotor});
+MotorGroup rightChassis ({backRightDriveMotor, middleRightDriveMotor, frontRightDriveMotor, upRightDriveMotor});
 
-//Digitally Builds the Chassis
+
 std::shared_ptr<ChassisController> drive =
 	ChassisControllerBuilder()
 		//.withMotors(leftChassis,rightChassis)
@@ -84,18 +89,24 @@ std::shared_ptr<ChassisController> drive =
 		) */
 		.build();
 
+
+
+
 Motor intakeMotor(16);
 Motor catapultMotor (11);
 ADIButton catapultLimit ('A');
-
+bool wingCheck;
+bool pistonExtended;
 
 
 //Runs initialization code. This occurs as soon as the program is started. All other competition modes are blocked by initialize; it is recommended to keep execution time for this mode under a few seconds.
 void initialize() {
+	wingCheck=true;
+	pistonExtended = false;
+
 	//pros::lcd::initialize();
    	sylib::initialize();
-
-
+	//Digitally Builds the Chassis
 }
 
 
@@ -121,14 +132,8 @@ void autonomous() {
 //Runs the operator control code. This function will be started in its own task with the default priority and stack size whenever the robot is enabled via the Field Management System or the VEX Competition Switch in the operator control mode. If no competition control is connected, this function will run immediately following initialize(). If the robot is disabled or communications is lost, the operator control task will be stopped. Re-enabling the robot will restart the task, not resume it from where it left off.
 void opcontrol() {
 	lv_init();
-	if (selectedProfile==0){
-		pros::screen::set_pen(COLOR_BLUE);
-    	pros::screen::print(pros::E_TEXT_MEDIUM, 3, "Gaston");
-	} 
-	else if (selectedProfile==1){
-		pros::screen::set_pen(COLOR_BLUE);
-    	pros::screen::print(pros::E_TEXT_MEDIUM, 3, "Patrick");
-	}
+
+	
 
 	
 	// Joystick to read analog values for tank or arcade control.
@@ -154,20 +159,26 @@ void opcontrol() {
 
 		ControllerButton intakeOutButton(ControllerDigital::R2);
 		ControllerButton intakeInButton(ControllerDigital::R1);
-		ControllerButton catapultButton(ControllerDigital::L1);
-		ControllerButton catapultProgressionButton(ControllerDigital::L2);
+		ControllerButton catapultButton(ControllerDigital::L2);
+		ControllerButton catapultProgressionButton(ControllerDigital::L1);
 		ControllerButton catapultButtonBack(ControllerDigital::up);
-		//ControllerButton indexCloseThreeButton(ControllerDigital::Y);
+		ControllerButton wingButton(ControllerDigital::Y);
+		ControllerButton armButton(ControllerDigital::right);
+		pros::ADIDigitalOut leftWing (WING_LEFT);
+		pros::ADIDigitalOut rightWing (WING_RIGHT);
+		pros::ADIDigitalOut arm (ARM);
 		//pros::ADIDigitalIn catapultLimit (CATA_PORT);
+		
 
 
-		//pros::ADIDigitalOut index (INDEX_PORT);
+		//pros::ADIDigitalOut leftWing (INDEX_PORT);
 		//pros::ADIDigitalOut endgame (ENDGAME_PORT);
 	pros::screen::set_pen(COLOR_BLUE);
     pros::screen::print(pros::E_TEXT_MEDIUM, 3, "%d",rightChassis.getActualVelocity());
 	pros::screen::print(pros::E_TEXT_MEDIUM, 3,"%d", leftChassis.getActualVelocity());
 
-
+		
+		
 		if (catapultButton.isPressed()) {
 			if (catapultProgressionButton.isPressed()){
 				catapultMotor.moveVoltage(12000);
@@ -194,6 +205,41 @@ void opcontrol() {
 		else {
         	intakeMotor.moveVoltage(0);
     	}
+		if (wingButton.isPressed() && !pistonExtended) {
+            leftWing.set_value(true); // Extend the piston
+            pistonExtended = true; // Set the piston state to extended
+        } else if (!wingButton.isPressed() && pistonExtended) {
+            // If the button is released and the piston is extended, retract it
+            leftWing.set_value(false); // Retract the piston
+            pistonExtended = false; // Set the piston state to retracted
+        }
+		/*
+		if (wingButton.isPressed()) {
+			//leftWing.set_value(true);
+			//rightWing.set_value(true);
+			//pros::delay(5000);
+
+    	
+		
+			
+			if (wingCheck==true){
+				leftWing.set_value(true);
+				rightWing.set_value(true);
+				wingCheck=false;
+				pros::delay(5000);
+			}
+			else {
+				leftWing.set_value(false);
+				rightWing.set_value(false);
+				wingCheck=true;
+				pros::delay(5000);
+			}
+		}
+		if (armButton.isPressed()) {
+			arm.set_value(true);
+			pros::delay(5000);
+		}	
+		*/
 	
     	
 		
@@ -202,7 +248,7 @@ void opcontrol() {
 		// Additionally, joystick values, motor telemetry, etc. all updates every 10 ms.
 		
 		pros::delay(10);
-	
+		
 	}
 	
 }
