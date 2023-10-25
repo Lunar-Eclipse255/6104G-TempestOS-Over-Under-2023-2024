d3c6df6c8ac546ef1166165f903a42a43c98da87
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-10-25 12:43:23 -0400

    error

--- src/Autons/rightBlueAutoProgram.cpp
+++ src/Autons/rightBlueAutoProgram.cpp
@@ -8,21 +8,26 @@
 //using namespace okapi; might be required
 
 void rightBlueAuton(void) {
-    drive->setMaxVelocity(150);
+    drive->setMaxVelocity(120);
     intakeMotor.moveVoltage(-12000); //might be reversed
     pros::delay(200);
     intakeMotor.moveVoltage(12000);
     pros::delay(200);
     drive->turnAngle(-1_deg);
     intakeMotor.moveVoltage(0);
-	drive->moveDistance(130_cm);
-    drive->turnAngle(68_deg); //≈90 degrees
+	drive->moveDistance(127_cm);
+    drive->turnAngle(63_deg); //≈90 degrees
+    drive->setMaxVelocity(400);
     intakeMotor.moveVoltage(-12000);
     drive->moveDistance(18.5_cm);
+    drive->setMaxVelocity(90);
     pros::delay(200);
     intakeMotor.moveVoltage(0);
-    drive->moveDistance(-21_cm);
+    drive->moveDistance(-19.5_cm);
     drive->turnAngle(45_deg); //≈90 degrees
-    drive->moveDistance(130_cm);
+    drive->setMaxVelocity(135);
+    drive->moveDistance(145_cm);
+    drive->moveDistance(-5_cm);
+    drive->turnAngle(-65_deg);
 
 }
