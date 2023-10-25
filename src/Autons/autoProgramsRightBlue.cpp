065e2be21849822da2f130cd7ac28dca12079316
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-10-24 20:13:18 -0400

    auto code

--- src/Autons/autoProgramsRightBlue.cpp
+++ src/Autons/autoProgramsRightBlue.cpp
@@ -8,21 +8,27 @@
 using namespace okapi;
 
 void rightBlueAuton(void) {
+    drive->setMaxVelocity(100);
     intakeMotor.moveVoltage(-12000); //might be reversed
     pros::delay(200);
     intakeMotor.moveVoltage(12000);
     pros::delay(200);
-    drive->turnAngle(-5_deg);
+    drive->turnAngle(-1_deg);
     intakeMotor.moveVoltage(0);
-	drive->moveDistance(130_cm);
-    drive->turnAngle(70_deg); //≈90 degrees
-    drive->moveDistance(50_cm);
+	drive->moveDistance(132.5_cm);
+    drive->turnAngle(68_deg); //≈90 degrees
     intakeMotor.moveVoltage(-12000);
+    drive->moveDistance(25.5_cm);
     pros::delay(200);
     intakeMotor.moveVoltage(0);
+    drive->moveDistance(-15_cm);
+    drive->turnAngle(45_deg); //≈90 degrees
+    drive->moveDistance(60_cm);
+    /*
     drive->moveDistance(-30_cm);
     drive->turnAngle(22_deg);
     drive->moveDistance(40_cm);
     drive->turnAngle(22_deg);
     drive->moveDistance(45_cm);
+    */
 }
