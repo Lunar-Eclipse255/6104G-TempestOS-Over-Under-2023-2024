fafcbb0d9e80f721870cb5491651d12cdc1030d0
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2023-10-26 13:32:39 -0400

    added code for leftBlueOneAuton

--- src/Autons/leftBlueAutoProgram.cpp
+++ src/Autons/leftBlueAutoProgram.cpp
@@ -8,6 +8,29 @@
 using namespace okapi;
 
 void leftBlueOneAuton(void) {
-	//pros::screen::set_pen(COLOR_BLUE);
-    //pros::screen::print(pros::E_TEXT_MEDIUM, 3, "Blue Flag");
+	drive->setMaxVelocity(120);
+    intakeMotor.moveVoltage(-12000); //might be reversed
+    pros::delay(200);
+    intakeMotor.moveVoltage(12000);
+    pros::delay(200);
+    drive->turnAngle(1_deg);
+    intakeMotor.moveVoltage(0);
+	drive->moveDistance(127_cm);
+    drive->turnAngle(-60_deg); //≈90 degrees
+    drive->setMaxVelocity(400);
+    intakeMotor.moveVoltage(-12000);
+    drive->moveDistance(18.5_cm);
+    drive->moveDistance(-14_cm);
+    intakeMotor.moveVoltage(0);
+    drive->moveDistance(14_cm);
+    drive->setMaxVelocity(90);
+    pros::delay(200);
+    intakeMotor.moveVoltage(0);
+    drive->moveDistance(-14_cm);
+    drive->turnAngle(-45_deg); //≈90 degrees
+    drive->setMaxVelocity(135);
+    drive->moveDistance(122_cm);
+    drive->moveDistance(-20_cm);
+    drive->turnAngle(65_deg);
+    drive->moveDistance(-110_cm);
 }
