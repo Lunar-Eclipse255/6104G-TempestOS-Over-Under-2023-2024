47600dc2dad8afcdb5543098458dc74378243875
Lunar-Eclipse255 <101727280+Lunar-Eclipse255@users.noreply.github.com>
2023-11-19 20:57:16 -0500

    code

--- src/functions/MotorFunctions.cpp
+++ src/functions/MotorFunctions.cpp
@@ -4,5 +4,25 @@
 
 class MotorFunctions{
     public: 
-        int port;
+        MotorFunctions(okapi::Motor &motor, okapi::ControllerDigital In, okapi::ControllerDigital Out){
+           okapi::Motor motor = motor;
+        }
+
+    /*
+        MotorFunctions(Motor motor, okapi::ControllerDigital In, okapi::ControllerDigital Out){
+            ControllerButton inButton(In);
+            ControllerButton outButton(Out);
+            if (inButton.isPressed()) {
+                motor.moveVoltage(12000);
+            } 
+            //else if the intakeOut button is pressed it gives the intake -12000 mV
+            else if (outButton.isPressed()) {
+                motor.moveVoltage(-12000);
+            }
+            //else it stops powering the intake motor
+            else {
+                motor.moveVoltage(0);
+            }
+        }
+        */
 };
