31e2404dba7f31e0499cf0fbe4a1a195353bd2ef
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2024-01-10 19:10:53 -0500

    change

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,7 @@
 #define WING_RIGHT 'C'
 #define ARM 'D'
 
-Motor intakeMotor(-5);
-Motor catapultMotor (7);
-ADIButton catapultLimit ('A');
+
 
 //Declares variables for state checks.
 bool wingCheckLeft;
@@ -24,6 +22,10 @@ bool blockerCheck;
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+Motor intakeMotor(-5);
+Motor catapultMotor (7);
+ADIButton catapultLimit ('A');
 
 // drive motors
 pros::Motor backLeftDriveMotor(10, pros::E_MOTOR_GEARSET_18); // left front motor. port 12, reversed
@@ -93,7 +95,7 @@ lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors
  */
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
-    chassis.calibrate(); // calibrate sensors
+    //chassis.calibrate(); // calibrate sensors
     wingCheckLeft=false;
 	wingCheckRight=false;
 	armCheck=false;
@@ -146,7 +148,7 @@ ASSET(leftAWP_txt);
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    chassis.moveToPose(2, 0, 0, 5000);
+    //chassis.moveToPose(2, 0, 0, 5000);
     chassis.follow(leftAWP_txt, 15, 4000, false);
 }
 
@@ -155,14 +157,137 @@ void autonomous() {
  */
 void opcontrol() {
     // controller
-    
+	pros::ADIDigitalOut leftWing (WING_LEFT);
+	pros::ADIDigitalOut rightWing (WING_RIGHT);
+	pros::ADIDigitalOut arm (ARM);
+    pros::ADIDigitalOut blocker (BLOCKER);
     // loop to continuously update motors
     while (true) {
+
+        ControllerButton intakeOutButton(ControllerDigital::R2);
+		ControllerButton intakeInButton(ControllerDigital::R1);
+		ControllerButton catapultButton(ControllerDigital::L2);
+		ControllerButton catapultProgressionButton(ControllerDigital::L1);
+		ControllerButton catapultButtonBack(ControllerDigital::up);
+		ControllerButton wingOutLeftButton(ControllerDigital::left);
+		ControllerButton wingInLeftButton(ControllerDigital::right);
+		ControllerButton wingOutRightButton(ControllerDigital::A);
+		ControllerButton wingInRightButton(ControllerDigital::Y);
+        ControllerButton blockerUpButton(ControllerDigital::down);
+		ControllerButton blockerDownButton(ControllerDigital::B);
+        
+
+        //pros::ADIDigitalIn catapultLimit (CATA_PORT);
+
+
+		//pros::ADIDigitalOut leftWing (INDEX_PORT);
+		//pros::ADIDigitalOut endgame (ENDGAME_PORT);
+	//pros::screen::set_pen(COLOR_BLUE);
+    //pros::screen::print(pros::E_TEXT_MEDIUM, 3, "%d",rightChassis.getActualVelocity());
+	//pros::screen::print(pros::E_TEXT_MEDIUM, 3,"%d", leftChassis.getActualVelocity());
+
         // get joystick positions
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
         // move the chassis with curvature drive
         chassis.curvature(rightX, leftY);
+        //Checks if the button for catapult is pressed
+		if (catapultButton.isPressed()) {
+			//Checks if the button for catapult progression is pressed, if so gives the catapultMotor 12000 mV
+			if (catapultProgressionButton.isPressed()){
+				catapultMotor.moveVoltage(12000);
+			}
+			//if the catapult limit switch is pressed it stops the motor
+			else if (catapultLimit.isPressed()){
+				catapultMotor.moveVoltage(0);
+			}
+			else  {
+				//else the motor is given 12000 mV
+				catapultMotor.moveVoltage(12000);
+			}
+		} //else if the catapult back button it gives -12000 mV
+		else if (catapultButtonBack.isPressed()) {
+        	catapultMotor.moveVoltage(-12000);
+		}
+		//else its stops powering the motor
+		else{
+			catapultMotor.moveVoltage(0);
+		}
+		//if the intakeIn button is pressed it gives the intake 12000 mV
+		if (intakeInButton.isPressed()) {
+        	intakeMotor.moveVoltage(12000);
+    	} 
+		//else if the intakeOut button is pressed it gives the intake -12000 mV
+		else if (intakeOutButton.isPressed()) {
+        	intakeMotor.moveVoltage(-12000);
+		}
+		//else it stops powering the intake motor
+		else {
+        	intakeMotor.moveVoltage(0);
+    	}
+		//If the wingOutButton is pressed and the wings aren't already out it extends 
+		if (wingOutLeftButton.isPressed()) {
+			if (wingCheckLeft==false){
+				leftWing.set_value(true);
+				wingCheckLeft=true;
+			}
+		}
+		//Else if the wingOutButton is pressed and the wings aren't already in it extends 
+		else if (wingInLeftButton.isPressed()) {
+			if (wingCheckLeft){
+				leftWing.set_value(false);
+				wingCheckLeft=false;
+			}
+		}
+		if (wingOutRightButton.isPressed()) {
+			if (wingCheckRight==false){
+				rightWing.set_value(true);
+				wingCheckRight=true;
+			}
+		}
+		//Else if the wingOutButton is pressed and the wings aren't already in it extends 
+		else if (wingInRightButton.isPressed()) {
+			if (wingCheckRight){
+				rightWing.set_value(false);
+				wingCheckRight=false;
+			}
+		}
+        if (blockerUpButton.isPressed()) {
+			if (blockerCheck==false){
+				blocker.set_value(true);
+				blockerCheck=true;
+			}
+		}
+		//Else if the wingOutButton is pressed and the wings aren't already in it extends 
+		else if (blockerDownButton.isPressed()) {
+			if (blockerCheck){
+				blocker.set_value(false);
+				blockerCheck=false;
+			}
+		}
+		/*
+		if (armOutButton.isPressed()) {
+			if (armCheck==false){
+				arm.set_value(true);
+				arm.set_value(true);
+				armCheck=true;
+			}
+
+		}	
+		else if (armInButton.isPressed()) {
+			if (armCheck){
+				arm.set_value(false);
+				arm.set_value(false);
+				armCheck=false;
+			}
+		}
+		*/
+	
+    	
+		
+		
+		// Wait and give up the time we don't need to other tasks.
+		// Additionally, joystick values, motor telemetry, etc. all updates every 10 ms.
         // delay to save resources
         pros::delay(10);
     }
