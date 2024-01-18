625edfeb8063b8a8b0d060f5d9a1cda6b8763acb
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2024-01-18 09:18:09 -0500

    named auton wrong

--- src/Autons/rightBlueOneAutoProgram.cpp
+++ src/Autons/rightBlueOneAutoProgram.cpp
@@ -2,18 +2,19 @@
 #include "display.hpp"
 #include "motors.h"
 #include "main.h"
-#include "paths.hpp"
-
-//#include "okapi/api.hpp"
-
-//using namespace okapi; might be required
+#include "okapi/api.hpp"
+using namespace okapi;
 
 void rightBlueOneAuton(void) {
+	driveChassis->setMaxVelocity(120);
 	intakeMotor.moveVoltage(-12000);
-	pros::delay(3);
+	pros::delay(200);
     intakeMotor.moveVoltage(12000);
-	driveChassis->moveDistance(2.2_ft);
-	driveChassis->moveDistance(-2_ft);
-	intakeMotor.moveVoltage(0);
-	driveChassis->moveDistance(-2_ft);
+	driveChassis->moveDistance(20_cm);
+	intakeMotor.moveVoltage(500);
+	driveChassis->moveDistance(-106_cm);
+	driveChassis->turnAngle(-43_deg);
+	rightWing.set_value(true);
+	driveChassis->moveDistance(-45_cm);
+	driveChassis->turnAngle(-23_deg);
 }
