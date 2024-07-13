581ab07213d46726e8f7f3b271e5e06e37694c4e
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2024-07-13 16:50:51 +0200

    used linguist-vendored to remove libaries we didn't make as well as un hiding scrap code

---
+++ LVGL Scrap/Button.cpp
@@ -0,0 +1,55 @@
+
+//Creates and Aligns the Red Auton Check Boxes
+    //lv_obj_t* redOption1 = lv_cb_create(redTab, NULL);
+    //lv_obj_t* redOption2 = lv_cb_create(redTab, NULL);
+    //lv_obj_align(redOption1, NULL, LV_ALIGN_IN_TOP_LEFT, 10,10);
+    //lv_obj_align(redOption2, NULL, LV_ALIGN_OUT_BOTTOM_LEFT,10,20);
+    //Labels the Red Auton Check Boxes
+    //lv_cb_set_text(redOption1, "Red Autonomous");
+    //lv_cb_set_text(redOption2, "Disabled Autonomous");
+    /*
+    //Creates and Aligns the Blue Auton Check Boxes
+    lv_obj_t* blueOption1 = lv_cb_create(blueTab, NULL);
+    lv_obj_t* blueOption2 = lv_cb_create(blueTab, NULL);
+    lv_obj_align(blueOption1, NULL, LV_ALIGN_IN_TOP_LEFT, 10,10);
+    lv_obj_align(blueOption2, NULL, LV_ALIGN_OUT_BOTTOM_LEFT,10,20);
+
+     //Labels the Blue Auton Check Boxes
+    lv_cb_set_text(blueOption1, "Blue Autonomous");
+    lv_cb_set_text(blueOption2, "Disabled Autonomous");
+
+
+    //Creates and Aligns the Skills Check Boxes
+    lv_obj_t* skillsOption1 = lv_cb_create(skillsTab, NULL);
+    lv_obj_t* skillsOption2 = lv_cb_create(skillsTab, NULL);
+    lv_obj_align(skillsOption1, NULL, LV_ALIGN_IN_TOP_LEFT, 10,10);
+    lv_obj_align(skillsOption2, NULL, LV_ALIGN_OUT_BOTTOM_LEFT,10,20);
+
+    //Labels the Skills Check Boxes
+    lv_cb_set_text(skillsOption1, "Programming Skills");
+    lv_cb_set_text(skillsOption2, "Driver Skills");
+    */
+
+
+
+
+/*void setupLVGL() {
+    // Initialize LVGL
+    lv_init();
+
+    // Create the custom screen
+    myScreen = lv_obj_create(lv_scr_act(), NULL);
+    lv_obj_set_size(myScreen, lv_obj_get_width(lv_scr_act()), lv_obj_get_height(lv_scr_act())); // Set screen size to maximum
+
+    // Create a button
+    button = lv_btn_create(myScreen, NULL);
+    lv_obj_align(button, NULL, LV_ALIGN_CENTER, 0, 0);
+
+    // Create a label
+    label = lv_label_create(myScreen, NULL);
+    lv_label_set_text(label, "Hello, LVGL!");
+    lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, -50);
+
+    // Load the custom screen
+    lv_scr_load(myScreen);
+}*/
---
+++ LVGL Scrap/Gauge.cpp
@@ -0,0 +1,14 @@
+	 /*Describe the color for the needles*/
+	/*
+    static lv_color_t needle_colors[1];
+    needle_colors[0] = LV_COLOR_BLUE;
+
+	
+    lv_obj_t * gauge1 = lv_gauge_create(lv_scr_act(), NULL);
+    lv_gauge_set_needle_count(gauge1, 1, needle_colors);
+    lv_obj_set_size(gauge1, 150, 150);
+    lv_obj_align(gauge1, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
+	
+    int batteryPercentage = pros::battery::get_capacity();
+    lv_gauge_set_value(gauge1, 0, batteryPercentage); 
+	*/
